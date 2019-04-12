3aad0fb5ad9ae49b59f861241006c22bafe0bc64
David DarkVamprism <DarkVamprism+Github@gmail.com>
2019-04-12 16:54:43 +0800

    Implemented Koolalas idea of allowing the user to select the two words to mirror. Thanks Koolala :)

--- mirrorEverything.js
+++ mirrorEverything.js
@@ -1,9 +1,17 @@
 // Author GroxicTinch
 document.getElementById("main").innerHTML = "Mirror Code Loaded";
 document.getElementById("main").style = "color: limegreen; text-align: center;";
+document.getElementById("log").value = "";
 msg("Awaiting input", "ready");
 
-var mirrorDir = -1; // 0 = X, 1 = Y, 2 = Z    10 = Swap all words "right" to "left" and vice-versa
+/*  0 = X, 1 = Y, 2 = Z
+    10 = Swap all words "right" to "left" and vice-versa
+    20 = Swamp given leftword with rightword
+ */
+
+var mirrorDir = -1;  
+var wordLeft = "";
+var wordRight = "";
 
 // Anyland Commands START
 // When ... then tell web MESSAGE
@@ -22,6 +30,14 @@ function AnylandTold(data, isAuthority) {
     } else if(data == "mirrorleftright") {
       mirrorDir = 10
       AnylandRequestThing(); // This calls AnylandGetThing
+    } else if(data.substring(1,9) == "wordleft " && data.length >= 10) {
+      wordLeft = data.substring(10);
+    } else if(data.substring(1,10) == "wordright " && data.length >= 11) {
+      wordRight = data.substring(11);
+    } else if(data == "swapwords" && wordLeft != "" && wordRight != "") {
+      // Thank you to Koolala for the suggestion of allowing the user to specify words :)
+      mirrorDir = 20
+      AnylandRequestThing(); // This calls AnylandGetThing
     }
   } else {
     msg("Unauthorized user attempt", "noauth");
@@ -32,7 +48,9 @@ function AnylandGetThing(json) {
   if(mirrorDir >= 0 && mirrorDir <= 2) {
     mirror(json);
   } else if(mirrorDir == 10){
-    mirrorleftright(json);
+    mirrorLeftRight(json);
+  } else if(mirrorDir == 10){
+    mirrorWords(json, wordLeft, wordRight);
   }
 }
 // Anyland Commands END
@@ -73,8 +91,12 @@ function mirror(json) {
   return false;
 }
 
-function mirrorleftright(json) {
-  msg("Progress: Swapping words \"Left\" and \"Right\"");
+function mirrorLeftRight(json) {
+  return mirrorWords(json, "left", "right");
+}
+
+function mirrorWords(json, wordLeft, wordRight) {
+  msg("Progress: Swapping words '" + wordLeft + "' and '" + wordRight + "'");
   if(json == "") {
     msg("Progress: Aborted, no json", "abort");
     details("User is not editing an object, Aborted");
@@ -89,7 +111,7 @@ function mirrorleftright(json) {
         var scripti;
         if(JSONobj["p"][parti]["s"][statei].hasOwnProperty("b")) {
           for(scripti = 0; scripti < JSONobj["p"][parti]["s"][statei]["b"].length; scripti++) {
-            JSONobj["p"][parti]["s"][statei]["b"][scripti] = swapLeftRight(JSONobj["p"][parti]["s"][statei]["b"][scripti]);
+            JSONobj["p"][parti]["s"][statei]["b"][scripti] = swapWords(JSONobj["p"][parti]["s"][statei]["b"][scripti], wordLeft, wordRight);
           }
         }
       }
@@ -121,9 +143,20 @@ function sendTell() {
   }
 }
 
-function swapLeftRight(scriptLine) {
-  scriptLine = scriptLine.replace(/left/g, "RIGHT").replace(/right/g, "left").replace(/RIGHT/g, "right");
-  return scriptLine;
+function swapWords(inString, wordLeft, wordRight) {
+  inString = replaceAll(inString, wordLeft, "TEMP");
+  inString = replaceAll(inString, wordRight, wordLeft);
+  inString = replaceAll(inString, "TEMP", wordRight);
+
+  return inString;
+}
+
+function replaceAll(inString, replaceWhat, replaceTo){
+  // Solution found here at https://stackoverflow.com/a/17820079
+  replaceWhat = replaceWhat.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  var re = new RegExp(replaceWhat, 'g');
+
+  return inString.replace(re,replaceTo);
 }
 
 function AnylandSetThing(json) {
